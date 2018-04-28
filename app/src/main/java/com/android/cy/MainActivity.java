554cbe2f7cbcc7221ee6f1516845ffb55022eca2
zhxh <zhuxiaohui@niuguwang.com>
2018-04-28 17:52:54 +0800

    dialog_time_range_picker

--- app/src/main/java/com/android/cy/MainActivity.java
+++ app/src/main/java/com/android/cy/MainActivity.java
@@ -1,13 +1,33 @@
 package com.android.cy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tvPushTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvPushTime = (TextView) findViewById(R.id.tvPushTime);
+
+        findViewById(R.id.setTimeLayout).setOnClickListener(view -> {
+
+            TimeRangePickerDialog dialog = new TimeRangePickerDialog(MainActivity.this, tvPushTime.getText().toString(), new TimeRangePickerDialog.ConfirmAction() {
+                @Override
+                public void onLeftClick() {
+                }
+
+                @Override
+                public void onRightClick(String startAndEndTime) {
+                    tvPushTime.setText(startAndEndTime);
+                }
+            });
+
+            dialog.show();
+
+        });
     }
 }
