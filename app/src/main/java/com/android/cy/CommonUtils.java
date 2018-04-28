0cca3323eb97f90cc322983b4077df8b3b8a663f
zhxh <zhuxiaohui@niuguwang.com>
2018-04-28 17:44:03 +0800

    CommonUtils

---
+++ app/src/main/java/com/android/cy/CommonUtils.java
@@ -0,0 +1,53 @@
+package com.android.cy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by zhxh on 2018/4/28.
+ */
+
+public class CommonUtils {
+    public static boolean isNull(String checkStr) {
+
+        boolean result = false;
+
+        if (null == checkStr){
+
+            result = true;
+        } else {
+            if (checkStr.length() == 0) {
+
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public static boolean isNull(List<?> list) {
+
+        boolean result = false;
+
+        if (null == list){
+
+            result = true;
+        } else {
+            if (list.size() == 0) {
+
+                result = true;
+            }
+        }
+        return result;
+    }
+    public static List<String> getRegEx(String input, String regex) {
+        List<String> stringList = new ArrayList<>();
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(input);
+        while (m.find())
+            stringList.add(m.group());
+
+        return stringList;
+    }
+}
